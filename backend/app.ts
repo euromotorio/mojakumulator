87217e5d5186ef328f4cf823b10e3685d0b7c9b1
euromotorio <euromotorio123@gmail.com>
2024-10-02 09:47:16 +0200

    Update app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -20,7 +20,7 @@ app.use("/api/users", userRouter);
 
 app.use(errorHandler);
 
-const PORT = 3000;
+const PORT = process.env.PORT | 3000;
 
 mongoose.connect(MONGODB_URI!).then((db) => {
 	app.listen(PORT, () => {
