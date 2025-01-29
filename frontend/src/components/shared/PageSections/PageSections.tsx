5a809539662a61d7283b818aef2d21458e0a708c
Nusret <nusretwazowski@gmail.com>
2025-01-29 11:29:35 +0100

    g

--- frontend/src/components/shared/PageSections/PageSections.tsx
+++ frontend/src/components/shared/PageSections/PageSections.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { PageData } from "../../../util/types";
 import PageSection from "./PageSection/PageSection";
 
@@ -7,9 +7,23 @@ interface PageSectionProps {
 }
 
 const PageSections: FC<PageSectionProps> = ({ data }) => {
+	const [sortedData, setSortedData] = useState<Array<PageData>>([]);
+
+	useEffect(() => {
+		if (data) {
+			const sorted = [...data].sort((a, b) => {
+				const numA = parseInt(a.subBrand.match(/^\d+/)?.[0] || "0", 10);
+				const numB = parseInt(b.subBrand.match(/^\d+/)?.[0] || "0", 10);
+				return numA - numB;
+			});
+
+			setSortedData(sorted);
+		}
+	}, [data]);
+
 	return (
 		<>
-			{data.map((item) => (
+			{sortedData.map((item) => (
 				<PageSection data={item} key={item.subBrand} />
 			))}
 		</>
